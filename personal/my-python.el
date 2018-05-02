90815f1ddd625d1545cd7e17d4ca0a762e56944a
Loïc Estève <loic.esteve@ymail.com>
2018-05-02 10:23:28 +0200

    elpy is not used by the spacemacs python layer

--- personal/my-python.el
+++ personal/my-python.el
@@ -1,9 +1,3 @@
-(require 'elpy)
-(elpy-enable)
-
-;; At the time of writing, jedi way faster than rope
-(setq elpy-rpc-backend "jedi")
-
 ;; Use ipython rather than plain python for the python shell
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i --simple-prompt --matplotlib")
