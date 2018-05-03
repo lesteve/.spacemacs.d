f03a558fae09f4c41a68af1863cc89623eb2d197
Loïc Estève <loic.esteve@ymail.com>
2018-05-03 09:03:01 +0200

    Use default Adobe font

--- personal/my-miscellaneous.el
+++ personal/my-miscellaneous.el
@@ -1,5 +1,3 @@
-(add-to-list 'default-frame-alist '(font . "-outline-Bitstream Vera Sans Mono-normal-normal-normal-mono-12-*-*-*-c-*-iso8859-1"))
-
 (server-mode 1)
 
 ;; I don't want to automatically clean up trailing spaces
