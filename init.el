06f7d36270e561ac0e8b2f0cc91f35473c09920e
Loïc Estève <loic.esteve@ymail.com>
2018-08-14 13:44:51 +0200

    Add ipython-notebook layer

--- init.el
+++ init.el
@@ -35,7 +35,7 @@ values."
      html
      yaml
      python
-     ipyton-notebook
+     ipython-notebook
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
@@ -59,7 +59,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(zenburn-theme ein)
+   dotspacemacs-additional-packages '(zenburn-theme)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
