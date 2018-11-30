b6d099c8e65cb93b7e7606dc3f5a096a14aedf92
Loïc Estève <loic.esteve@ymail.com>
2018-11-30 15:45:22 +0100

    Remove commented out code.

--- personal/my-email.el
+++ personal/my-email.el
@@ -95,8 +95,6 @@
 (setq mu4e-change-filenames-when-moving t)
 
 ;;; Show only interesting folders in main view
-;; (setq mu4e-use-maildirs-extension t)
-
 (setq mu4e-maildirs-extension-custom-list
       '("/inria/Inbox" "/inria/SED" "/inria/Lists/WillowSierra" "/inria/Lists/hpc-big-data"
         "/ymail/Inbox" "/ymail/github" "/ymail/github/dask" "/ymail/github/scikit-learn"
