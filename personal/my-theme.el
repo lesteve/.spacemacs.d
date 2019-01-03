311e468f591079a786439b3a4e31b21fef918ad9
Loïc Estève <loic.esteve@ymail.com>
2019-01-03 15:32:22 +0100

    Move from zenburn to default spacemacs-dark theme.

--- personal/my-theme.el
+++
@@ -1,8 +0,0 @@
-(zenburn-with-color-variables
-  (custom-theme-set-faces
-   'zenburn
-   ;; Remove flymake squigly lines which prevents from reading the
-   ;; code. Use different background instead.
-   `(flymake-errline ((t (:underline nil))))
-   `(flymake-warnline ((t (:underline nil))))))
-
