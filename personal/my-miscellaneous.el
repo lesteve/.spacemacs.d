cc57a7d03a6723e6b2c8a86cc323fdd667d7cce1
Loïc Estève <loic.esteve@ymail.com>
2019-02-15 16:08:30 +0100

    Remove commented out prelude-specific lines

--- personal/my-miscellaneous.el
+++ personal/my-miscellaneous.el
@@ -1,9 +1,5 @@
 (server-mode 1)
 
-;; I don't want to automatically clean up trailing spaces
-;; TODO
-;; (setq prelude-clean-whitespace-on-save nil)
-
 ;; So I can use accents when I feel like writing some French
 (require 'iso-transl)
 
