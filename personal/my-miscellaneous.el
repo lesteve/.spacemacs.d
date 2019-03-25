30e5e60c5a0c88acdccf92c55cf1c43d7b5a8354
Loïc Estève <loic.esteve@ymail.com>
2019-03-25 18:09:22 +0100

    Use builtin spacemacs variable to start emacs server.

--- personal/my-miscellaneous.el
+++ personal/my-miscellaneous.el
@@ -1,7 +1,3 @@
-;; Start emacs server if not running already
-(require 'server)
-(unless (server-running-p) (server-start))
-
 ;; So I can use accents when I feel like writing some French
 (require 'iso-transl)
 
