6c693e8efde88eee53d8aceee8bebaf849a62c59
Loïc Estève <loic.esteve@ymail.com>
2022-06-16 11:16:59 +0200

    Set font size in point rather than pixel

--- init.el
+++ init.el
@@ -267,7 +267,7 @@ It should only modify the values of Spacemacs settings."
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 14
+                               :size 10.0
                                :weight normal
                                :width normal)
 
