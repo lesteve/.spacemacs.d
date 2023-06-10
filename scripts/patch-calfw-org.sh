4993b3321bc7f10a29417ecd60ea89e39380bf59
Loïc Estève <loic.esteve@ymail.com>
2023-06-10 07:24:26 +0200

    Dowload calfw-org patch from PR

--- scripts/patch-calfw-org.sh
+++ scripts/patch-calfw-org.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 file_to_patch=$(find ~/.emacs.d/ -name calfw-org.el)
 cp ${file_to_patch}{,.bak}
-patch ${file_to_patch} scripts/calfw-org.el.patch
+
+wget https://github.com/kiwanami/emacs-calfw/pull/134.patch -O /tmp/calfw-org.el.patch 
+patch ${file_to_patch} /tmp/calfw-org.el.patch
 
