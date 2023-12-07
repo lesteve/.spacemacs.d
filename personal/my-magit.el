135f753e39487d657d5456fbc8462d56c19bf6c2
Loïc Estève <loic.esteve@ymail.com>
2023-12-07 14:49:37 +0100

    Add simple magit/forge setup

--- personal/my-magit.el
+++ personal/my-magit.el
@@ -1,3 +1,13 @@
 ;; Using e on a diff just does diff (working directory vs HEAD) and not stage
 ;; with a 3-way merge view (HEAD, index, working directory):
 (setq magit-ediff-dwim-show-on-hunks t)
+
+;; This has the token for magit/forge
+(setq auth-sources '("~/.authinfo.gpg"))
+
+(setq  forge-topic-list-limit '(100 . 0))
+
+(setq  forge-topic-list-limit '(100 . -10))
+
+(with-eval-after-load 'magit
+  (require 'forge))
