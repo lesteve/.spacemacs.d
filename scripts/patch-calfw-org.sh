06ef19dfb23ff36c332f766f1a2a96e5c8f866dd
Loïc Estève <loic.esteve@ymail.com>
2024-02-06 13:51:28 +0100

    Make calfw-org patch more robust

--- scripts/patch-calfw-org.sh
+++ scripts/patch-calfw-org.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-file_to_patch=$(find ~/.emacs.d/ -name calfw-org.el)
+emacs_version=$(emacs --version | head -n1 | perl -pe 's@\D+([\d.]+)@\1@')
+file_to_patch=$(find ~/.emacs.d/elpa/$emacs_version -name calfw-org.el)
 cp ${file_to_patch}{,.bak}
 
 wget https://github.com/kiwanami/emacs-calfw/pull/134.patch -O /tmp/calfw-org.el.patch 
