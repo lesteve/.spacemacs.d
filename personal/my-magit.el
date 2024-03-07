183c63e92c9e76a7d53737520b4c8e30c378e712
Loïc Estève <loic.esteve@ymail.com>
2024-03-07 13:46:44 +0100

    [magit] forge tweak

--- personal/my-magit.el
+++ personal/my-magit.el
@@ -5,9 +5,20 @@
 ;; This has the token for magit/forge
 (setq auth-sources '("~/.authinfo.gpg"))
 
-(setq  forge-topic-list-limit '(100 . 0))
-
-(setq  forge-topic-list-limit '(100 . -10))
-
 (with-eval-after-load 'magit
-  (require 'forge))
+  ;; Set smaller graphqlItemLimit to avoid HTTP 502 Bad gateway or HTTP 504 Gateway time-out
+  ;; Following https://github.com/magit/forge/issues/69#issuecomment-1572402440
+  (magit-set "forge.graphqlItemLimit" "50")
+  (require 'forge)
+  ;; Follow recommendation from https://github.com/magit/forge/issues/367#issuecomment-1848723122
+  (setq forge-notifications-github-kludge 'pending-again)
+
+  ;; (require 'code-review)
+  ;; (setq code-review-auth-login-marker 'forge)
+
+  ;; Limit to 50 open topics and show 10 closed topics (hidden by default)
+  (setq  forge-topic-list-limit '(50 . -10))
+
+)
+
+
