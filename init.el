eb9bb109f59b181198b97726253167ca16aff7fc
Loïc Estève <loic.esteve@ymail.com>
2024-07-21 07:51:19 +0200

    Add quarto-mode package

--- init.el
+++ init.el
@@ -88,8 +88,9 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(org-caldav jupyter disable-mouse visual-fill-column calfw calfw-org noflet code-review meson-mode khalel)
-
+   dotspacemacs-additional-packages '(
+       org-caldav jupyter disable-mouse visual-fill-column
+       calfw calfw-org noflet code-review meson-mode khalel quarto-mode)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
 
